a6c74c9dd6b943bc5b709a6acedbd5fb167d7425
blakeedwards823 <blakeedwards823@gmail.com>
2020-05-17 18:27:19 -0400

    updated data processing script to describe errors and success on class basis

--- raw_data_to_train_set/training_data_flow_from_directory.py
+++ raw_data_to_train_set/training_data_flow_from_directory.py
@@ -26,12 +26,12 @@ for i in range(0, len(classes)):
     classes[i] = re.sub(str(collected_data_path)+str('/'), "", classes[i])
 classes = sorted(classes, key=str.lower)
 
+processed = [0,0,0]
+failed = [0,0,0]
 for i in range(0, len(classes)):
     curr_data_path = os.path.join(collected_data_path, classes[i])
     print(f"processing  class: {classes[i]}")
     csvs = glob(os.path.join(curr_data_path, "*.csv"))
-    processed = 0
-    failed = 0
     for k in range(0, len(csvs)):
         print(f"processing {csvs[k]}")
         raw_data = pd.read_csv(csvs[k])
@@ -47,16 +47,18 @@ for i in range(0, len(classes)):
             std = np.std(sample_diffs, axis=0)
             mean = np.mean(sample_diffs, axis=0)
             diff = abs((1000/15)-mean)
+            idx = classes.index(classes[i])
             if diff < mean_diff_threshold:
-                print(f"Processing sample with mean: {mean}")
-                processed = processed + 1
+                # print(f"Processing sample with mean: {mean}")
+                processed[idx] += 1
                 training_samples.append(sample)
                 target = np.zeros(3)
                 target[classes.index(classes[i])] = 1
                 training_targets.append(target)
             else:
                 print(f"Dropping sample with mean: {mean}")
-                failed = failed + 1
+                print(idx)
+                failed[idx] += 1
                 # print(sample_diffs)
                 # plt.subplot(211)
                 # plt.title("Milliseconds between samples")
@@ -69,11 +71,15 @@ for i in range(0, len(classes)):
                 # plt.plot(np.arange(1, window_size + 1), raw_data['accelerometerAccelerationZ(G)'][j:j + window_size], 'b')
                 # plt.ylim([-3.5, 3.5])
                 # plt.show()
-print(f'\nSuccesfully processed {(processed)/float((failed+processed))*100}% of the dataset')
-print(f'Total samples: {failed+processed}')
-print(f'Failed samples: {failed}')
-print(f'Successful samples: {processed}')
 
+print(f'\nClasses: {classes}')
+print(f'Successful: {processed}')
+print(f'Failed: {failed}')
+
+print(f'\nSuccesfully processed {(sum(processed))/float((sum(failed)+sum(processed)))*100}% of the dataset')
+print(f'Total samples: {sum(failed)+sum(processed)}')
+print(f'Failed samples: {sum(failed)}')
+print(f'Successful samples: {sum(processed)}')
 
 np.save("training_samples", np.asarray(training_samples))
 np.save("training_targets", np.asarray(training_targets))
