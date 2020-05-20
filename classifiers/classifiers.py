fffe9b64ebe60b0a2e091ca44c0985d03c8c2d07
blakeedwards823 <blakeedwards823@gmail.com>
2020-05-20 02:30:45 -0400

    update to confusion matrix and accuracy metrics

--- classifiers/classifiers.py
+++ classifiers/classifiers.py
@@ -8,30 +8,17 @@ from sklearn.metrics import confusion_matrix
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation, BatchNormalization
 
 epochs = 50
 batch_size = 32
-num_classes = 6
-dustbin_label_loc = 0
+num_classes = 7
 img_rows, img_cols = 45, 3
 
 # load generated samples
-classes = ['dustbin', 'lay', 'run', 'sit', 'stair', 'walk']
+classes = ['drive', 'dustbin', 'lay', 'run', 'sit', 'stair', 'walk']
 train_x = np.load("../raw_data_to_train_set/training_samples.npy")
 train_y = np.load("../raw_data_to_train_set/training_targets.npy")
-# train_x = train_x[:,:,1:]
-print(train_x.shape)
-
-# create set of dustbin samples
-num_dustbin_samples = 500
-dustbin_samples = np.random.uniform(-3,3,size=(num_dustbin_samples,45,3))
-dustbin_labels = np.zeros((num_dustbin_samples, num_classes))
-dustbin_labels[:, dustbin_label_loc] = 1
-
-# generate random noise for dust bin class
-train_x = np.concatenate((train_x, dustbin_samples))
-train_y = np.concatenate((train_y, dustbin_labels))
 
 # todo: split off test set when we have larger dataset
 (x_train, y_train), (x_test, y_test) = (train_x, train_y), (train_x, train_y)
@@ -48,14 +35,24 @@ x_test = x_test.reshape(-1, 45, 3, 1)
 input_shape = (45, 3, 1)
 
 model = Sequential()
-model.add(Conv2D(16, kernel_size=(3, 3),
+model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
+model.add(Conv2D(64, kernel_size=(2, 1),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
+model.add(Dense(64))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(32))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(Dense(num_classes, activation='softmax'))
+# SparseCategoricalCrossentropy
 model.compile(loss=tensorflow.keras.losses.categorical_crossentropy,
-              optimizer=tensorflow.keras.optimizers.Adadelta(lr=0.01),
+              optimizer=tensorflow.keras.optimizers.Adam(lr=0.05),
               metrics=['accuracy'])
 model.summary()
 
@@ -72,13 +69,14 @@ y_pred = model.predict(x_test)
 y_pred = [np.argmax(y) for y in y_pred]
 y_test_max = [np.argmax(y) for y in y_test]
 
-cf_matrix = confusion_matrix(y_test_max, y_pred)
+cf_matrix = confusion_matrix(y_test_max, y_pred, normalize='true')
 
 sns.heatmap(cf_matrix, annot=True)
 plt.title(f"{classes}")
 plt.show()
 
 print(classes)
+np.set_printoptions(suppress=True)
 print(cf_matrix)
 
 
